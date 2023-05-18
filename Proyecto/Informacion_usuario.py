187445f406ef8a4814c0220aceff99ccdd1c0441
Dlassoc <99360471+Dlassoc@users.noreply.github.com>
2023-05-17 23:15:32 -0500

    Informacion_usuario.py

--- Proyecto/Informacion_usuario.py
+++ Proyecto/Informacion_usuario.py
@@ -1,26 +1,35 @@
+import re
 import tkinter as tk
-from tkinter import messagebox
 
 
 class Usuario:
-    datos: dict = {}
+    datos = {} #Esto será variable de clase para que todos los metodos puedan acceder a la info
 
-    def guardar_datos(self, nombre, apellido, email, contraseña, contraseña_verificacion, saldo):
-        if len(contraseña) < 6:
+    def guardar_datos(self, nombre, apellido, email, contraseña, contraseña_verificacion, saldo, numero_cuenta):
+        if not re.match("^[a-zA-Z]+$", nombre):
+            tk.messagebox.showerror("Error", "El nombre solo debe contener letras")
+        elif not re.match("^[a-zA-Z]+$", apellido):
+            tk.messagebox.showerror("Error", "El apellido solo debe contener letras")
+        elif len(contraseña) < 6:
             tk.messagebox.showerror("Error", "La contraseña debe tener al menos 6 caracteres")
+        elif contraseña != contraseña_verificacion:
+            tk.messagebox.showerror("Error", "Las contraseñas no coinciden")
+        elif 'email' in self.__class__.datos and self.__class__.datos['email'] == email:
+            tk.messagebox.showerror("Error", "El correo electrónico ya está registrado")
         else:
-            if contraseña != contraseña_verificacion:
-                tk.messagebox.showerror("Error", "Las contraseñas no coinciden")
-            else:
-                self.__class__.datos['nombre'] = nombre
-                self.__class__.datos['apellido'] = apellido
-                self.__class__.datos['email'] = email
-                self.__class__.datos['contraseña'] = contraseña_verificacion
-                self.__class__.datos["saldo"] = saldo
-                tk.messagebox.showinfo("Usuario creado con éxito")
-                return True
+            self.__class__.datos['nombre'] = nombre
+            self.__class__.datos['apellido'] = apellido
+            self.__class__.datos['email'] = email
+            self.__class__.datos['contraseña'] = contraseña_verificacion
+            self.__class__.datos["saldo"] = saldo
+            self.__class__.datos["numero_cuenta"] = numero_cuenta
+            return True
 
-    def comparar_datos(self, email, contraseña):
+    @classmethod
+    def correo_existe(cls, email):
+        return email in cls.datos
+
+    def validar_datos(self, email, contraseña):
         if not self.__class__.datos:
             tk.messagebox.showerror("Acceso denegado", "El correo electrónico o la contraseña son incorrectos")
         else:
@@ -28,7 +37,7 @@ class Usuario:
             stored_password = self.__class__.datos.get('contraseña')
 
             if stored_email is None or stored_password is None:
-                tk.messagebox.showerror("Acceso denegado", "Ingresa la informacion ,no dejes espacios en blanco")
+                tk.messagebox.showerror("Acceso denegado", "Ingresa la información, no dejes espacios en blanco")
                 return False
             elif email == stored_email and contraseña == stored_password:
                 tk.messagebox.showinfo("Acceso concedido", "¡Inicio de sesión exitoso!")
