643f67a5b9e64d848145b4f62e12c88614c9c00e
Dlassoc <99360471+Dlassoc@users.noreply.github.com>
2023-05-18 02:22:31 -0500

    Update Registrar_usuario.py

--- Proyecto/Registrar_usuario.py
+++ Proyecto/Registrar_usuario.py
@@ -1,7 +1,7 @@
 import random
 import tkinter as tk
 from PIL import ImageTk, Image
-from Proyecto.Informacion_usuario import Usuario
+from Proyecto.informacion_usuario import Usuario
 
 
 class RegisterWindow(tk.Toplevel):
@@ -106,3 +106,4 @@ class RegisterWindow(tk.Toplevel):
                                           numero_cuenta):
                 tk.messagebox.showinfo("Registro exitoso", "Se ha registrado con éxito")
                 self.destroy()
+
